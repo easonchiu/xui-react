a1fd61ba92dbe918decf7906b5cb6b57de780e22
Eason chiu <76670758@qq.com>
2016-12-08 01:08:09 +0800

    e

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import R from './styles/reset.scss'
 
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 
-import Comp from './componets/Comp/index.jsx'
+import Body from './componets/Body/index.jsx'
 
 
 render(
-	<Comp />,
+	<Body />,
 	document.getElementById('app')
 );
