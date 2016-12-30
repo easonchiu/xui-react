f1c7dcb994551531c92f6ecbe85ba3db99945236
Eason chiu <76670758@qq.com>
2016-12-31 00:48:31 +0800

    e

--- src/Views/Panel/index.jsx
+++ src/Views/Panel/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
-import Cell, { CellTitle, CellRow, CellRowHeader, CellRowBody } from '../../Xui/Components/Cell'
+import Cell, { CellTitle, CellRow, CellRowHeader, CellRowBody, CellRowFooter } from '../../Xui/Components/Cell'
+import Button from '../../Xui/Components/Button'
 import Panel, { PanelHeader, PanelBody, PanelFooter } from '../../Xui/Components/Panel'
 import Icon from '../../Xui/Components/Icon'
 
@@ -28,6 +29,47 @@ class PanelView extends Component {
 			</Cell>
 		)
 
+		let cell2 = (
+			<Cell>
+				<CellRow>
+					<CellRowHeader>
+						<label>姓名</label>
+					</CellRowHeader>
+					<CellRowBody>
+						<input type="text" placeholder="请输入" />
+					</CellRowBody>
+					<CellRowHeader>
+						<label>性别</label>
+					</CellRowHeader>
+					<CellRowBody>
+						<select>
+							<option value="1">男</option>
+							<option value="2">女</option>
+						</select>
+					</CellRowBody>
+				</CellRow>
+				<CellRow>
+					<CellRowHeader>
+						<label>住址</label>
+					</CellRowHeader>
+					<CellRowBody>
+						<input type="text" placeholder="请输入" />
+					</CellRowBody>
+				</CellRow>
+				<CellRow>
+					<CellRowHeader>
+						<label>注册码</label>
+					</CellRowHeader>
+					<CellRowBody>
+						<input type="text" placeholder="请输入" />
+					</CellRowBody>
+					<CellRowFooter>
+						<Button type="primary" mini>验证</Button>
+					</CellRowFooter>
+				</CellRow>
+			</Cell>
+		)
+
 		return (
 			<div>
 				<CellTitle title="模块" />
@@ -44,6 +86,18 @@ class PanelView extends Component {
 					<PanelFooter>
 						<span>底部提示文字</span>
 					</PanelFooter>
+				</Panel>
+				
+				<CellTitle title="录入" />
+				<Panel>
+					<PanelHeader>
+						<h2>模块名称</h2>
+					</PanelHeader>
+					<PanelBody>
+						
+						{ cell2 }
+
+					</PanelBody>
 				</Panel>
 				
 			</div>
